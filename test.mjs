4362aa922fb0fc0bde78ef4e1571a1ffd12e61a2
FacundesPedro <pedro.f.facundes1@gmail.com>
2022-03-01 17:38:37 -0300

    removed test.mjs

--- test.mjs
+++
@@ -1,8 +0,0 @@
-import {createServer} from 'http'
-
-createServer((req,res)=>{
-    req.method.
-    import 
-    res.write('Ok')
-    res.end()
-}).listen(5003,()=>console.log('Server on on port 5003'))
